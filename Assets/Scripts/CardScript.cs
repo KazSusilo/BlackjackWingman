5f980416f7ee97e5825c15a3f4e95db8f12921cf
KazSusilo <97570310+KazSusilo@users.noreply.github.com>
2025-03-26 18:22:35 -0400

    Minor changes in CardScript
    * Styling changes

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -2,20 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardScript : MonoBehaviour
-{
+public class CardScript : MonoBehaviour {
     // Value of card, 2 of spades = 2, etc
     private int value = 0;
 
-    // Get the value of card
-    public int GetValue() {
-        return value;
-    }
 
-    // Set value of card 
-    public void SetValue(int newValue) {
-        value = newValue;
-    }
+    // cardValue acessor functions
+    public int GetValue() { return value; }
+    public void SetValue(int newValue) { value = newValue; }
 
     // Set sprite of card gameObject to 'newSprite'
     public void SetSprite(Sprite newSprite) {
